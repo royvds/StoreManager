061addfb96bd1dd7e508b3dc074c07ec617201e0
Roy <i379346@fhict.nl>
2019-05-30 19:16:44 +0200

    Setup jest & enzyme to work with webpack and babel, created one sample test

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import routes from './routes'
 import App from './components/app'
---
+++ src/tests/components/login/login.test.js
@@ -0,0 +1,42 @@
+import { shallow, mount, render } from 'enzyme'
+import React from 'react'
+import Login from '../../../components/login/login'
+import Cookies from 'universal-cookie'
+const cookies = new Cookies()
+
+require("babel-polyfill")
+
+describe('Login Component', () => {
+
+  beforeAll(() => {
+    const div = document.createElement('div')
+    window.domNode = div
+    document.body.appendChild(div)
+  })
+
+  it('Test login by jwt cookie', async () => {
+    const wrapper = mount(<Login />, { attachTo: window.domNode })
+    const mailInput = wrapper.find('#loginForm-email')
+    const passwordInput = wrapper.find('#loginForm-password')
+    const submitButton = wrapper.find('#loginForm-submit')
+
+    cookies.remove('jwt')
+
+    mailInput.getDOMNode().value = 'vdsteenroy@hotmail.nl'
+    passwordInput.getDOMNode().value = 'Password1'
+    await wrapper.find('#loginForm').simulate('submit')
+
+    function timeout(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async function sleep(fn, ...args) {
+      await timeout(2000);
+      return fn(...args);
+    }
+
+    await sleep(function(){
+      expect(cookies.get('jwt').accessToken)
+    })
+  })
+
+})
